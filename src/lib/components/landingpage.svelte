820cfb425d00bd8d9e5e535f7870cdd5da870d10
KlinsBt <klinsbt@gmail.com>
2024-04-29 02:53:20 +0200

    i

--- src/lib/components/landingpage.svelte
+++ src/lib/components/landingpage.svelte
@@ -64,7 +64,7 @@ import c11 from "../images/carousel/i11.png";
 }
 
 .img-container > h1 {
-    font-family: KristenITC;
+    font-family: KristenITC, "Segoe UI", "Helvetica Neue", sans-serif !important;
     font-size: 50px;
     color: black;
     position: absolute;
